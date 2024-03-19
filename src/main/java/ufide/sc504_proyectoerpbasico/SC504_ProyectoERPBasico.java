1905859717c5a30a09500ede77405d0e7790a341
Roberto-Cervantes <rcervantes00828@ufide.ac.cr>
2024-03-18 19:46:45 -0600

    Introducción del font end
    Se crea el proyecto Java Maven

---
+++ src/main/java/ufide/sc504_proyectoerpbasico/SC504_ProyectoERPBasico.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ufide.sc504_proyectoerpbasico;
+
+/**
+ *
+ * @author User
+ */
+public class SC504_ProyectoERPBasico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
